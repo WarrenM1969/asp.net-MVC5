b60260f355612264f8aceb156d9bfda82b59f1dd
WarrenM1969 <warrenm@impacttech.com>
2016-11-28 15:03:48 -0600

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mvcOnTraQ2.Startup))]
+namespace mvcOnTraQ2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
